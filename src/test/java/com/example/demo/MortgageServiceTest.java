13438c2deadbae070fcaa53b25af1403df9872fd
Egor Kurakov <ekurakov@alfabank.ru>
2020-11-25 18:10:15 +0300

    add jacoco

--- src/test/java/com/example/demo/MortgageServiceTest.java
+++ src/test/java/com/example/demo/MortgageServiceTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.util.Assert;
 
 class MortgageServiceTest {
 
